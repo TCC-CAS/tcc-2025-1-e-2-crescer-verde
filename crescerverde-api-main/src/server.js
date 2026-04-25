839014f9a2fb06f2faf5d05479ae372302b31200
Lima <v.lima@sap.com>
2026-04-25 16:06:10 -0300

    feat: cards translucidos, fix scroll quiz, fix CORS Authorization

--- crescerverde-api-main/src/server.js
+++ crescerverde-api-main/src/server.js
@@ -7,7 +7,11 @@ const { authRoutes, courseRoutes, userRoutes, courseContentRoutes, courseProgres
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // request logger must run early so all incoming traffic is logged
 app.use(loggerMiddleware);
